3601b22a7c2a1f9093b800ab332b02880ba747fe
HappyRedCat <HappyRedCat@users.noreply.github.com>
2024-02-27 16:26:04 +0100

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,87 +1,92 @@
 import { Component } from 'react';
-import { Layout } from './Layout';
-import ModalWindow from './Modal/Modal';
-import SearchBar from './Searchbar/Searchbar';
-import ImageGallery from './ImageGallery/ImageGallery';
-import Loader from './Loader/Loader';
-import * as ImageService from './api';
-import { Notification } from './Layout';
-import toast, { Toaster } from 'react-hot-toast';
-import Button from './Button/Button';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AppStyled } from './App.styled';
+import { Searchbar } from './Searchbar/Searchbar';
+import { Button } from './Button/Button';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { fetchImages } from '../utils/service';
+import { Dna } from 'react-loader-spinner';
+import { Modal } from './Modal/Modal';
 
 export class App extends Component {
   state = {
+    images: [],
     query: '',
-    hits: [],
-    error: false,
+    page: 1,
     isLoading: false,
     totalHits: 0,
-    page: 1,
+    showModal: false,
+    activeImage: null,
   };
 
-  onSubmit = query => {
-    if (!query) return;
-    this.setState({
-      hits: [],
-      page: 1,
-      query,
-    });
-  };
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-    ) {
-      try {
-        this.setState({ isLoading: true, error: false });
-        {
-          const {
-            data: { hits },
-          } = await ImageService.fetchImages({
-            query: this.state.query,
-            page: this.state.page,
-          });
-
-          this.setState(prevState => ({
-            hits: [...prevState.hits, ...hits],
-          }));
-          if (hits.length > 0) {
-            toast.success('We found images for you');
+    if (prevState.query !== query || prevState.page !== page) {
+      this.setState({ isLoading: true });
+      fetchImages(query, page)
+        .then(({ hits, totalHits }) => {
+          if (!totalHits) {
+            console.log(totalHits, hits);
+            toast.error('There are no images for your request');
+            return;
           }
-        }
-      } catch (error) {
-        this.setState({ error: true });
-      } finally {
-        this.setState({ isLoading: false });
-      }
+          const results = hits.map(
+            ({ tags, id, webformatURL, largeImageURL }) => ({
+              tags,
+              id,
+              smallImage: webformatURL,
+              largeImage: largeImageURL,
+            })
+          );
+
+          this.setState(({ images }) => {
+            return { images: [...images, ...results], totalHits };
+          });
+        })
+        .catch(error => {
+          toast.error('There are no images for your request');
+        })
+        .finally(() => this.setState({ isLoading: false }));
     }
   }
 
-  changePage = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
+  submitHandler = query => {
+    window.scrollTo({ behavior: 'smooth', top: 0 });
+    this.setState({
+      query,
+      images: [],
+      page: 1,
+      totalHits: 0,
     });
   };
 
-  render() {
-    const { error, hits, isLoading, totalHits } = this.state;
-    return (
-      <Layout>
-        <SearchBar onSubmit={this.onSubmit} />
-        {hits.length === 0 && !error && (
-          <Notification>Try to find some images</Notification>
-        )}
-        {isLoading && <Loader />}
-        {error && (
-          <Notification>Whoops! Error! Please reload this page!</Notification>
-        )}
-        {hits.length > 0 && <ImageGallery images={hits} />}
+  onLoadMoreButton = () => {
+    this.setState(({ page }) => ({
+      page: page + 1,
+    }));
+  };
 
-        {hits.length !== totalHits && <Button changePage={this.changePage} />}
-        <ModalWindow />
-        <Toaster />
-      </Layout>
+  onImageClick = (activeImage = null) => {
+    this.setState({ activeImage });
+  };
+
+  render() {
+    const { isLoading, images, totalHits, activeImage } = this.state;
+    return (
+      <AppStyled>
+        <Searchbar onSubmit={this.submitHandler} />
+        <ImageGallery images={images} openModal={this.onImageClick} />
+        {totalHits > images.length && !isLoading && (
+          <Button onLoadMoreButton={this.onLoadMoreButton} />
+        )}
+        {isLoading && <Dna wrapperStyle={{ margin: '0 auto' }} />}
+        {activeImage && (
+          <Modal image={activeImage} onClose={this.onImageClick}></Modal>
+        )}
+        <ToastContainer />
+      </AppStyled>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import './index.css';
 
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from 'GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={{}}>
-      <App />
-      <GlobalStyle />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
